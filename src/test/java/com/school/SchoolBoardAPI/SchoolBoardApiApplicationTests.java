41463b18de16e8d915e535649c19927e9faf1734
Shivaraju G <ktdyshivu@gmail.com>
2024-01-16 09:37:54 +0530

    School board api added to global repo

---
+++ src/test/java/com/school/SchoolBoardAPI/SchoolBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.SchoolBoardAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
